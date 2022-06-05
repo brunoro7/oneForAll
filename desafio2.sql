d2ede33a82511a79228a3342a3538dff26e834dc
brunoro7 <bruno.ro7c@gmail.com>
2022-06-05 06:12:42 -0300

    feat: desafio2.sql
    Criar uma QUERY para exibir três colunas:
    
    - A primeira coluna deve exibir a quantidade total de canções. Dê a essa coluna o alias "cancoes".
    - A segunda coluna deve exibir a quantidade total de artistas e deverá ter o alias "artistas".
    - A terceira coluna deve exibir a quantidade de álbuns e deverá ter o alias "albuns".

---
+++ desafio2.sql
@@ -0,0 +1,9 @@
+SELECT
+	COUNT(song.`id`) AS `cancoes`,
+    COUNT(DISTINCT artist.`id`) AS `artistas`,
+    COUNT(DISTINCT album.`id`) AS `albuns` 
+FROM SpotifyClone.song AS song
+INNER JOIN SpotifyClone.album AS album
+ON album.id = song.album_id
+INNER JOIN SpotifyClone.artist AS artist
+ON album.artist_id = artist.id;
